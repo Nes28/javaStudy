12e0b82604350ddc29b9cf07519034f71e4708b9
Shun N <torshunsuke@gmail.com>
2021-04-13 00:07:49 +0900

    gitignore調整

---
+++ dataType.project/src/IntType.java
@@ -0,0 +1,9 @@
+public class IntType {
+    public static void main(String[] args) throws Exception {
+        int minInt = Integer.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+
+        System.out.printf("int型の最小値 = %d %n" , minInt);
+        System.out.printf("int型の最大値 = %d %n" , maxInt);
+    }
+}
