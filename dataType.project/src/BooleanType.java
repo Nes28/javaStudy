02779a90e025c55c3d28e438ddb0c467bc4bd0ee
Shun N <torshunsuke@gmail.com>
2021-04-13 00:05:56 +0900

    gitignore調整

--- dataType.project/src/BooleanType.java
+++
@@ -1,9 +0,0 @@
-public class BooleanType {
-    public static void main(String[] args) throws Exception {
-        boolean hasElephantLongNose = true;
-        System.out.printf("象の鼻は長いか = %b%n", hasElephantLongNose);
-        boolean isJapanLargerThanAmerica = false;
-        System.out.printf("日本はアメリカより大きいか　= %b%n", isJapanLargerThanAmerica);
-
-    }
-}
--- dataType.project/src/ShortType.java
+++
@@ -1,9 +0,0 @@
-public class ShortType {
-    public static void main(String[] args) throws Exception {
-        short minShort = Short.MIN_VALUE;
-        short maxShort = Short.MAX_VALUE;
-
-        System.out.printf("Short型の最小値 = %d %n", minShort);
-        System.out.printf("Short型の最大値 = %d %n", maxShort);
-    }
-}
